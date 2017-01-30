90c35c2ad0ee9cb4eba2d63a0e163098a453912d
ari.quinones <ari.quinones@remuv.io>
2017-01-29 21:59:00 -0600

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ var app = express();
 app.use(bodyParser.json());
 // creating a todo
 app.post('/todos', (req,res) => {
-	console.log(req.body);
 	var todo = new Todo({
 		text: req.body.text
 	});
@@ -24,6 +23,13 @@ app.post('/todos', (req,res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	})
+})
 
 
 app.listen(3000, () => {
