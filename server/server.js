854f86ebc958950e5e588b2da33361a639e969e4
ari.quinones <ari.quinones@remuv.io>
2017-01-30 21:01:45 -0600

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 // Local imports
 var {mongoose} = require('./db/mongoose');
@@ -31,6 +32,22 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectId.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
 
 app.listen(3000, () => {
 	console.log('Started on port 3000')
@@ -38,3 +55,5 @@ app.listen(3000, () => {
 
 
 module.exports = {app};
+
+
