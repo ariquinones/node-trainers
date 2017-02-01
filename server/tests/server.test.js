298e4618bf09dba63b7e9d01b19f2b63ab8bfb14
ari.quinones <ari.quinones@remuv.io>
2017-01-31 22:48:17 -0600

    add tests for PATCH

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [
 	},
 	{
 		_id: new ObjectId(),
-		text: "Second test todo"
+		text: "Second test todo",
+		completed: true,
+		completedAt: 333
 	}
 ];
 
@@ -141,6 +143,40 @@ describe('DELETE /todos/:id', () => {
 	});
 })
 
+describe('UPDATE /todos/:id', () => {
+	it('should update the todo', (done) => {
+		var hexId = todos[0]._id.toHexString();
+		var text = "new text";
 
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				text: text,
+				completed: true
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.completed).toBe(true)
+				expect(res.body.todo.completedAt).toBeA('number')
+				expect(res.body.todo.text).toBe(text)
+			})
+			.end(done);
+	});
+
+	it('should clear completeAt when todo is not completed', (done) => {
+		var hexId = todos[1]._id.toHexString();
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				completed: false
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.completed).toBe(false)
+				expect(res.body.todo.completedAt).toNotExist();
+			})
+			.end(done);
+	});
+});
 
 
